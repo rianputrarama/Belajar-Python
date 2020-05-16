f9aee50188dbdde4f2e9886686f71caa080abcd1
rian <rianpr20@gmail.com>
2020-05-16 08:14:28 +0700

    list

---
+++ list_program.py
@@ -0,0 +1,8 @@
+# list, bisa membuat list dengan beberapa type data yang berbeda
+
+data_emp1 = ['Rian', 25, 'Software Enginer']
+print("Nama: %s\nUmur: %d\nJob Desc: %s\n"%(data_emp1[0],data_emp1[1],data_emp1[2]))
+
+colors = ['red', 'green', 'blue']
+print(colors[2])
+print(len(colors))
