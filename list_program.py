5711c695ee1ac71ceec61a37bab840a5c305d7c4
rian <rianpr20@gmail.com>
2020-05-17 22:59:39 +0700

    if the value in collection

--- list_program.py
+++ list_program.py
@@ -6,3 +6,6 @@ print("Nama: %s\nUmur: %d\nJob Desc: %s\n"%(data_emp1[0],data_emp1[1],data_emp1[
 colors = ['red', 'green', 'blue']
 print(colors[2])
 print(len(colors))
+
+if 'green' in colors:
+    print('i found green')
